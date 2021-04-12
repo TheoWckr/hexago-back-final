83fd079faf9443a56cf63d911cf42d42c052fbd6
Nicolas Albanel <nicolas.albanel@talan.com>
2021-04-12 18:07:42 +0200

    fix chat

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ dotEnv.config();
 
 let cors = require('cors');
 let app = express();
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
 
 // connect to Mongo daemon
 if (process.env.JEST_WORKER_ID !== undefined) {
@@ -103,41 +106,7 @@ app.use('/api/v0/badge', BadgeRouter);
 app.use('/api/v0/genre', GenreRouter);
 app.use('/api/v0/chat', ChatRouter);
 
-const http = require('http').createServer(app);
 
-// require the socket.io module
-const io = require('socket.io');
-
-
-
-const socket = io(http);
-//create an event listener
-
-
-app.use(morgan('combined', { stream: winston.stream }));
-//To listen to messages
-socket.on('connection', (socket)=>{
-  socket.on("newMessage", async function(msg, userId, chatId) {
-
-    //save chat to the database
-    let Chat = require('./models/chat');
-    const chat = await Chat.findById(chatId);
-    chat.messages.push({userId: userId, message: msg})
-    chat.save();
-
-    socket.broadcast.emit("message", msg, chat._id);
-  });
-  socket.on("writingMessage", async function(userId, chatId) {
-    let Chat = require('./models/chat');
-    const chat = await Chat.findById(chatId);
-    socket.broadcast.emit("writing", userId, chat._id, chat.userIdList);
-  });
-  socket.on("stopWritingMessage", async function(userId, chatId) {
-    let Chat = require('./models/chat');
-    const chat = await Chat.findById(chatId);
-    socket.broadcast.emit("stopWriting", userId, chat._id, chat.userIdList);
-  });
-});
 
 //wire up the server to listen to our port 500
 
--- start.js
+++ start.js
@@ -1,6 +1,48 @@
 const app = require('./app.js')
 const port = process.env.PORT;
+let morgan = require('morgan');
+const winston = require('./config/winston')
+const http = require('http').createServer(app);
 
-app.listen(port, ()=>{
+http.listen(port, ()=>{
     console.log('connected to port: '+ port)
+});
+
+
+// require the socket.io module
+const socket = require("socket.io")(http, {
+  cors: {
+    origin: "http://127.0.0.1:3000",
+  },
+  
+});
+
+
+// const socket = io(http);
+//create an event listener
+
+
+app.use(morgan('combined', { stream: winston.stream }));
+//To listen to messages
+socket.on('connection', (socket)=>{
+  socket.on("newMessage", async function(msg, userId, chatId) {
+
+    //save chat to the database
+    let Chat = require('./models/chat');
+    const chat = await Chat.findById(chatId);
+    chat.messages.push({userId: userId, message: msg})
+    chat.save();
+
+    socket.broadcast.emit("message", msg, chat._id);
+  });
+  socket.on("writingMessage", async function(userId, chatId) {
+    let Chat = require('./models/chat');
+    const chat = await Chat.findById(chatId);
+    socket.broadcast.emit("writing", userId, chat._id, chat.userIdList);
+  });
+  socket.on("stopWritingMessage", async function(userId, chatId) {
+    let Chat = require('./models/chat');
+    const chat = await Chat.findById(chatId);
+    socket.broadcast.emit("stopWriting", userId, chat._id, chat.userIdList);
+  });
 });
